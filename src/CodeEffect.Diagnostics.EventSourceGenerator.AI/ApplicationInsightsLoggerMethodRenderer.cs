50ecf0440c02ac3cab28bac4076bf74132ff7115
Fredrik Göransson <fredrik.goransson@gmail.com>
2017-04-27 08:48:23 +0200

    Added basic Application Insights project as extension to core

---
+++ src/CodeEffect.Diagnostics.EventSourceGenerator.AI/ApplicationInsightsLoggerMethodRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeEffect.Diagnostics.EventSourceGenerator.Model;
+using Microsoft.ApplicationInsights;
+
+namespace CodeEffect.Diagnostics.EventSourceGenerator.AI
+{
+    public class ApplicationInsightsLoggerMethosdRenderer : ILoggerImplementationMethodRenderer
+    {
+        public string Render(Project project, ProjectItem<LoggerModel> loggerProjectItem, EventModel eventModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TelemetryHelper
+    {
+        private TelemetryClient _client = new TelemetryClient();
+
+
+        public void LogEvent()
+        {
+            //_client.TrackEvent("Hello", new );
+        }
+    }
+}
