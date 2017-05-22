424e6682bcf1f7c55df03ceca2518a8b8429d632
Fredrik Göransson <fredrik.goransson@gmail.com>
2017-05-22 16:45:09 +0200

    Clean up Extensions discovery

--- src/CodeEffect.Diagnostics.EventSourceGenerator/Builders/ProjectExtensionsDiscoverBuilder.cs
+++ src/CodeEffect.Diagnostics.EventSourceGenerator/Builders/ProjectExtensionsDiscoverBuilder.cs
@@ -31,17 +31,7 @@ namespace CodeEffect.Diagnostics.EventSourceGenerator.Builders
             var referenceFiles = files.OfType(ProjectItemType.Reference)
                 .Where(r => ! System.IO.Path.GetFileName(r.Name).Equals(currentAssemblyFileName, StringComparison.InvariantCultureIgnoreCase))
                 .ToArray();
-            /*foreach (var file in extensionFiles)
-            {
-                LogMessage($"Found Extension file {file.Name}");
-                var extensions = CompileAndEvaluateExtensions(dynamicAssembly, file, referenceFiles);
-                foreach (var foundExtensions in extensions)
-                {
-                    LogMessage($"Compiled Extension {foundExtensions.GetType().FullName}");
 
-                }
-                builderExtensions.AddRange(extensions);
-            }*/
             LogMessage($"Scanning for Extensions in code source only");
             var compiledExtensions = CompileAndEvaluateExtensions(dynamicAssembly);
             foreach (var foundExtensions in compiledExtensions)
@@ -58,39 +48,7 @@ namespace CodeEffect.Diagnostics.EventSourceGenerator.Builders
             }
             builderExtensions.AddRange(extensionsInReferences);
             return builderExtensions.ToArray();
-        }
-
-        /*
-        private IExtension[] DiscoverExtensions(Assembly dynamicAssembly)
-        {
-            LogMessage($"Compiling possible logger builder extensions in referenced dlls");
-
-            var extensions = new List<IExtension>();
-            try
-            {
-                if (dynamicAssembly != null)
-                {
-
-                    AssemblyName.GetAssemblyName(dynamicAssembly)
-                    var references = referenceItems.Where(r => System.IO.Path.IsPathRooted(r.Name)).Select(r => AssemblyName.GetAssemblyName(dynamicAssembly.Name)).ToArray();
-
-                    var loaderType = dynamicAssembly.GetType("CodeEffect.Labs.ReferenceLoader.ExtensionLoader");
-                    var loader = Activator.CreateInstance(loaderType);
-                    var types = (loaderType?.GetMethod("GetExtensionTypes")?.Invoke(loader, new object[] { references }) as Type[]) ?? new Type[0];
-                    foreach (var type in types.Where(t => typeof(IExtension).IsAssignableFrom(t)))
-                    {
-                        var extension = (IExtension)Activator.CreateInstance(type);
-                        extensions.Add(extension);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                LogWarning($"Failed to compile/evaluate references - {ex.Message}\r\n{ex.StackTrace}");
-            }
-            return extensions.ToArray();
-        }
-        */
+        }        
 
         private IExtension[] FindExtensionsInReferences(IEnumerable<string> referenceFiles)
         {
@@ -163,56 +121,21 @@ namespace CodeEffect.Diagnostics.EventSourceGenerator.Builders
             var extensions = new List<IExtension>();
             try
             {
-                //                var complierHelper = new ComplierHelper();
-                //                this.PassAlongLoggers(complierHelper);
-
-                //                var loaderHelperDefinition = @"
-                //using System;
-                //using System.Collections.Generic;
-                //using System.Diagnostics;
-                //using System.Linq;
-                //using System.Reflection;
-                //using CodeEffect.Diagnostics.EventSourceGenerator.Model;
-
-                //namespace CodeEffect.Labs.ReferenceLoader
-                //{
-                //    public class ExtensionLoader
-                //    {
-                //        public Type[] GetExtensionTypes(AssemblyName[] references)
-                //        {
-                //            var types = new List<Type>();
-                //            foreach (var referencedAssembly in references)
-                //            {
-                //                var assembly = Assembly.Load(referencedAssembly);
-                //                Debug.WriteLine($""Loaded assembly {assembly.GetName().Name} with {assembly.GetTypes().Length} types"");
-                //                types.AddRange(assembly.GetTypes().Where(t => t.IsClass && !t.IsAbstract && typeof(IExtension).IsAssignableFrom(t)));
-                //            }
-                //            return types.ToArray();
-                //        }
-                //    }
-                //}";
-
-                //var compiledAssembly = complierHelper.Compile(cscToolPath, loaderHelperDefinition, referenceItems);
                 if (dynamicAssembly != null)
                 {
                     var types = dynamicAssembly.GetTypes().Where(t => t.IsClass && !t.IsAbstract && typeof(IExtension).IsAssignableFrom(t));
                     foreach (var type in types.Where(t => typeof(IExtension).IsAssignableFrom(t)))
                     {
-                        var extension = (IExtension) Activator.CreateInstance(type);
-                        extensions.Add(extension);
+                        try
+                        {
+                            var extension = (IExtension)Activator.CreateInstance(type);
+                            extensions.Add(extension);
+                        }
+                        catch (Exception ex)
+                        {
+                            LogWarning($"Failed to create instance of type {type?.Name} - {ex.Message}\r\n{ex.StackTrace}");
+                        }
                     }
-                    /*
-                    var references = referenceItems.Where(r => System.IO.Path.IsPathRooted(r.Name)).Select(r => AssemblyName.GetAssemblyName(r.Name)).ToArray();
-
-                    var loaderType = dynamicAssembly.GetType("CodeEffect.Labs.ReferenceLoader.ExtensionLoader");
-                    var loader = Activator.CreateInstance(loaderType);
-                    var types = (loaderType?.GetMethod("GetExtensionTypes")?.Invoke(loader, new object[] { references}) as Type[]) ?? new Type[0];
-                    foreach (var type in types.Where(t => typeof(IExtension).IsAssignableFrom(t)))
-                    {
-                        var extension = (IExtension)Activator.CreateInstance(type);
-                        extensions.Add(extension);
-                    }
-                    */
                 }
             }
             catch (Exception ex)
@@ -232,7 +155,6 @@ namespace CodeEffect.Diagnostics.EventSourceGenerator.Builders
                 var complierHelper = new ComplierHelper();
                 this.PassAlongLoggers(complierHelper);
 
-                //var compiledAssembly = complierHelper.Compile(cscToolPath, projectItem, referenceItems);                
                 var types = dynamicAssembly.GetTypes();
                 foreach (var type in types.Where(t => typeof(IExtension).IsAssignableFrom(t)))
                 {
