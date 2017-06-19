8c9c80631bcb31213d14003ba1a8732dc3ea4ad6
Fredrik Göransson <fredrik.goransson@gmail.com>
2017-06-19 09:16:47 +0200

    Removed ServiceFabric update backup

--- samples/CodeEffect.ServiceFabric.Auditing/ServiceFabricServicesBackup/PersonActor/Diagnostics/IActorDomainLogger.cs
+++
@@ -1,9 +0,0 @@
-namespace PersonActor.Diagnostics
-{
-	public interface IActorDomainLogger
-	{
-		void PersonActivated(string name, string title);
-		void PersonLoaded();
-		void TitleSet(string title); 
-	}
-}
--- samples/CodeEffect.ServiceFabric.Auditing/ServiceFabricServicesBackup/TitleService/Diagnostics/ITitleServiceLogger.cs
+++
@@ -1,7 +0,0 @@
-namespace TitleService.Diagnostics
-{
-	public interface ITitleServiceLogger
-	{
-	    void UpdateTitle(string person, string title);
-	}
-}
