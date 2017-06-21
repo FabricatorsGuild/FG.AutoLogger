5aa0ea50c85356bbd3d29416a12927140b94bdd3
Fredrik Göransson <fredrik.goransson@gmail.com>
2017-06-21 13:07:17 +0200

    Use CommandLine non-prerelease parser

--- src/CodeEffect.Diagnostics.EventSourceGenerator.Tool/Program.cs
+++ src/CodeEffect.Diagnostics.EventSourceGenerator.Tool/Program.cs
@@ -22,88 +22,51 @@ namespace CodeEffect.Diagnostics.EventSourceGenerator.Tool
                 args = line.Split(' ');
             }
 
-            var options = Parser.Default.ParseArguments<ConsoleOptions>(args);
-            options.WithParsed(t =>
+            var parsedOptions = new ConsoleOptions();
+            Parser.Default.ParseArguments(args, parsedOptions);
+
+            if (parsedOptions.Verbose) LogMessage($"Filename: {parsedOptions.ProjectFile}");
+
+            if (parsedOptions.ProjectFile == null)
             {
-                if (t.Verbose) LogMessage($"Filename: {t.ProjectFile}");
-
-                if (t.ProjectFile == null)
+                var possibleProjectFiles = System.IO.Directory.GetFiles("*.csproj");
+                if (possibleProjectFiles.Any())
                 {
-                    var possibleProjectFiles = System.IO.Directory.GetFiles("*.csproj");
-                    if (possibleProjectFiles.Any())
-                    {
-                        t.ProjectFile = possibleProjectFiles.First();
-                    }
+                    parsedOptions.ProjectFile = possibleProjectFiles.First();
                 }
+            }
 
-                if (!System.IO.File.Exists(t.ProjectFile))
+            if (!System.IO.File.Exists(parsedOptions.ProjectFile))
+            {
+                LogMessage($"The project file {parsedOptions.ProjectFile} could not be found", EventLevel.Critical);
+                return;
+            }
+
+            if (!System.IO.Path.IsPathRooted(parsedOptions.ProjectFile))
+            {
+                parsedOptions.ProjectFile = PathExtensions.GetAbsolutePath(parsedOptions.ProjectFile);
+            }
+            var projectFilePath = parsedOptions.ProjectFile;
+
+
+            var projectEventSourceGenerator = new ProjectEventSourceGenerator();
+            projectEventSourceGenerator.SetLogMessage(m => LogMessage(m, EventLevel.Informational));
+            projectEventSourceGenerator.SetLogWarning(w => LogMessage(w, EventLevel.Warning));
+            projectEventSourceGenerator.SetLogError(e => LogMessage(e, EventLevel.Error));
+
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var roslynDirectory = System.IO.Path.Combine(baseDirectory, "roslyn");
+
+            var project = projectEventSourceGenerator.Run(projectFilePath, roslynDirectory, parsedOptions.SaveChanges, parsedOptions.ForceUpdate);
+
+            if (parsedOptions.DisplayOutput)
+            {
+                foreach (var output in project.ProjectItems.OfType(
+                    ProjectItemType.EventSource,
+                    ProjectItemType.DefaultGeneratedEventSourceDefinition,
+                    ProjectItemType.EventSourceLoggerPartial,
+                    ProjectItemType.LoggerImplementation))
                 {
-                    LogMessage($"The project file {t.ProjectFile} could not be found", EventLevel.Critical);
-                    return;
-                }
-
-                if (!System.IO.Path.IsPathRooted(t.ProjectFile))
-                {
-                    t.ProjectFile = PathExtensions.GetAbsolutePath(t.ProjectFile);
-                }
-                var projectFilePath = t.ProjectFile;
-
-
-                var projectEventSourceGenerator = new ProjectEventSourceGenerator();
-                projectEventSourceGenerator.SetLogMessage(m => LogMessage(m, EventLevel.Informational));
-                projectEventSourceGenerator.SetLogWarning(w => LogMessage(w, EventLevel.Warning));
-                projectEventSourceGenerator.SetLogError(e => LogMessage(e, EventLevel.Error));
-
-                var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-                var roslynDirectory = System.IO.Path.Combine(baseDirectory, "roslyn");
-
-                var project = projectEventSourceGenerator.Run(projectFilePath, roslynDirectory, t.SaveChanges, t.ForceUpdate);
-
-                if (t.DisplayOutput)
-                {
-                    foreach (var output in project.ProjectItems.OfType(
-                        ProjectItemType.EventSource, 
-                        ProjectItemType.DefaultGeneratedEventSourceDefinition,
-                        ProjectItemType.EventSourceLoggerPartial,
-                        ProjectItemType.LoggerImplementation))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine($"{"".PadRight(40, '_')}");
-                        Console.WriteLine($"{"".PadRight(40, '=')}");
-                        Console.WriteLine($"File: {output.Name}");
-                        Console.WriteLine($"{"".PadRight(40, '_')}");
-
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine(output.Output);
-
-
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine($"{"".PadRight(40, '_')}");
-                        Console.WriteLine($"{"".PadRight(40, '=')}");
-                    }
-
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"{"".PadRight(40, '_')}");
-                    Console.WriteLine($"{"".PadRight(40, '=')}");
-                    Console.WriteLine($"File: {projectFilePath}");
-                    Console.WriteLine($"{"".PadRight(40, '_')}");
-
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"{"".PadRight(40, '_')}");
-                    Console.WriteLine($"{"".PadRight(40, '=')}");
-
-                }
-
-
-                if (t.GenerateSchema)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"Generating JSON Schema");
-
-                    var output = SchemaWriter.GenerateSchema(t.SaveChanges).GetAwaiter().GetResult();
-
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine($"{"".PadRight(40, '_')}");
                     Console.WriteLine($"{"".PadRight(40, '=')}");
@@ -117,15 +80,51 @@ namespace CodeEffect.Diagnostics.EventSourceGenerator.Tool
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine($"{"".PadRight(40, '_')}");
                     Console.WriteLine($"{"".PadRight(40, '=')}");
-
-                    Console.WriteLine($"Done generating JSON Schema");
                 }
 
-                if (t.Interactive)
-                {
-                    Console.ReadKey();
-                }
-            });
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{"".PadRight(40, '_')}");
+                Console.WriteLine($"{"".PadRight(40, '=')}");
+                Console.WriteLine($"File: {projectFilePath}");
+                Console.WriteLine($"{"".PadRight(40, '_')}");
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{"".PadRight(40, '_')}");
+                Console.WriteLine($"{"".PadRight(40, '=')}");
+
+            }
+
+
+            if (parsedOptions.GenerateSchema)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Generating JSON Schema");
+
+                var output = SchemaWriter.GenerateSchema(parsedOptions.SaveChanges).GetAwaiter().GetResult();
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{"".PadRight(40, '_')}");
+                Console.WriteLine($"{"".PadRight(40, '=')}");
+                Console.WriteLine($"File: {output.Name}");
+                Console.WriteLine($"{"".PadRight(40, '_')}");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(output.Output);
+
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{"".PadRight(40, '_')}");
+                Console.WriteLine($"{"".PadRight(40, '=')}");
+
+                Console.WriteLine($"Done generating JSON Schema");
+            }
+
+            if (parsedOptions.Interactive)
+            {
+                Console.ReadKey();
+            }
         }
 
         private static void LogMessage(string message, EventLevel eventLevel)
