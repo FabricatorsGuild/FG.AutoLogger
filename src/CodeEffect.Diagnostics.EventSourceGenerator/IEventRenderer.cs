f4ac0d5eb102bd66ded900228cf452d649fe46df
Fredrik Göransson <fredrik.goransson@gmail.com>
2017-06-21 12:49:38 +0200

    Bump

--- src/CodeEffect.Diagnostics.EventSourceGenerator/IEventRenderer.cs
+++
@@ -1,9 +0,0 @@
-using CodeEffect.Diagnostics.EventSourceGenerator.Model;
-
-namespace CodeEffect.Diagnostics.EventSourceGenerator
-{
-    public interface IEventRenderer
-    {
-        string Render(EventModel model, int index, EventSourcePrototype eventSource);
-    }
-}
