5a70ff25d11cb581437b52ff7bf1a266f3f2c0e6
Fredrik Göransson <fredrik.goransson@gmail.com>
2017-07-03 15:47:10 +0200

    Add OperationHolder to AI module

--- src/CodeEffect.Diagnostics.EventSourceGenerator.AI/LoggerImplementationMembersAITelemetryRendererExtension.cs
+++ src/CodeEffect.Diagnostics.EventSourceGenerator.AI/LoggerImplementationMembersAITelemetryRendererExtension.cs
@@ -6,7 +6,43 @@ namespace FG.Diagnostics.AutoLogger.AI
     {
         public string Render(Project project, ProjectItem<LoggerModel> loggerProjectItem)
         {
-            return @"private readonly Microsoft.ApplicationInsights.TelemetryClient _telemetryClient;";
+            return @"private readonly Microsoft.ApplicationInsights.TelemetryClient _telemetryClient;
+
+        public sealed class OperationHolder
+        {
+            public static void StartOperation(IOperationHolder<RequestTelemetry> aiOperationHolder)
+            {
+                OperationHolder.Current = new OperationHolder() {AIOperationHolder = aiOperationHolder};
+            }
+
+            public static IOperationHolder<RequestTelemetry> StopOperation()
+            {
+                var aiOperationHolder = OperationHolder.Current.AIOperationHolder;
+                OperationHolder.Current = null;
+
+                return aiOperationHolder;
+            }
+
+            private IOperationHolder<RequestTelemetry> AIOperationHolder { get; set; }
+
+            private static readonly string ContextKey = Guid.NewGuid().ToString();
+
+            public static OperationHolder Current
+            {
+                get { return (OperationHolder)CallContext.LogicalGetData(ContextKey); }
+                internal set
+                {
+                    if (value == null)
+                    {
+                        CallContext.FreeNamedDataSlot(ContextKey);
+                    }
+                    else
+                    {
+                        CallContext.LogicalSetData(ContextKey, value);
+                    }
+                }
+            }
+        }";
         }
     }
 }
