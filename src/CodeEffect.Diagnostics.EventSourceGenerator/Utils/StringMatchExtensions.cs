5fe82524453904fbf23223bd6b5355092d125e3c
Fredrik Göransson <fredrik.goransson@gmail.com>
2017-07-03 16:48:13 +0200

    Update DefaultEventSource naming

--- src/CodeEffect.Diagnostics.EventSourceGenerator/Utils/StringMatchExtensions.cs
+++ src/CodeEffect.Diagnostics.EventSourceGenerator/Utils/StringMatchExtensions.cs
@@ -8,6 +8,9 @@ namespace FG.Diagnostics.AutoLogger.Generator.Utils
 {
     public static class StringMatchExtensions
     {
+
+        private static readonly Regex RemoveNonWordStringRegex = new Regex(@"[^\w]", RegexOptions.Compiled);
+
         private static readonly Regex FindInitialLowercaseRegex = new Regex(@"\b([a-z])", RegexOptions.Compiled);
 
         private static readonly Regex HumanReadableStringRegex = new Regex(@"((?<=\p{Ll})\p{Lu}|\p{Lu}(?=\p{Ll}))", RegexOptions.Compiled);
@@ -29,6 +32,10 @@ namespace FG.Diagnostics.AutoLogger.Generator.Utils
             return Regex.IsMatch(that, regExPattern, options);
         }
 
+        public static string RemoveNonWordCharacters(this string that)
+        {
+            return RemoveNonWordStringRegex.Replace(that, "");
+        }
 
         public static string ToMD5(this byte[] that)
         {
